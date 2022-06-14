58d71c9cf7684d34f668c3352a913df302e6863c
Aaron Hamilton <aham9695@gmail.com>
2022-06-14 15:42:05 +0000

    Finish form response, however response does not show up in server logs what is the error?

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,23 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String name = request.getParameter("name-value");
+    String email = request.getParameter("email-value");
+    String country = request.getParameter("country-value");
+    String subject = request.getParameter("subject-value");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Submitted Name: " + name);
+    System.out.println("Submitted Email: " + email);
+    System.out.println("Submitted Country: " + country);
+    System.out.println("Submitted Subject: " + subject);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("Submitted Name: " + name);
+    response.getWriter().println("Submitted Email: " + email);
+    response.getWriter().println("Submitted Country: " + country);
+    response.getWriter().println("Submitted Subject: " + subject);
+
+    response.sendRedirect("https://ahamilton-sps-summer22.appspot.com/");
   }
 }
